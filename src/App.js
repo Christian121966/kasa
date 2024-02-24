8c800886510de04ee21ad3eb2d0e14b91178f432
Christian121966 <127527013+Christian121966@users.noreply.github.com>
2024-02-24 15:34:25 +0100

    P8-React

--- src/App.js
+++ src/App.js
@@ -22,4 +22,19 @@ function App() {
   );
 }
 
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  );
+}
+
 export default App;
---
+++ src/pages/accueil.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './accueil.scss'
+import Header from '../../components/header/Header'
+import Banner from '../../components/banner/Banner'
+import Gallery from '../../components/gallery/Gallery'
+import Footer from '../../components/footer/Footer'
+
+function accueil() {
+	return (
+		<div className='accueil'>
+			<Header />
+			<Banner />
+			<Gallery />
+			<Footer />
+		</div>
+	)
+}
+export default accueil;
