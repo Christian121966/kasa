7017a4345cd8975d58d43bc3c780bb8ec6149152
Christian121966 <127527013+Christian121966@users.noreply.github.com>
2024-04-02 18:22:27 +0200

    linear-gradient Card

--- src/pages/notFound/NotFound.js
+++ src/pages/notFound/NotFound.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
 import './NotFound.scss';
 import { Link } from 'react-router-dom';
 
@@ -11,13 +12,12 @@ function NotFound() {
       <h1 className='error'>
         404
       </h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
       <h5 className="retour">
         <Link to="/">Retourner sur la page d'accueil</Link>
-    </h5>
-      
-
+      </h5>
     </div>
+    <Footer />
     </>
   );
 }
