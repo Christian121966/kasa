aae723734e435be43bec5a313f7c87d3f1e5e2cc
Christian121966 <boissychristian@gmail.com>
2024-04-11 00:01:25 +0200

    Finalité

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,16 +1,17 @@
-
-
+import React from 'react';
 import './banner.scss';
+import bannerImage from '../../assets/Image source 2 (1).svg';
 
-function Banner({isVisible = true, showimage = false}) {
-    return (
-      <>
+function Banner({ isVisible = true, showimage = false }) {
+  return (
+    <>
       <section className={showimage ? 'banner-about' : 'banner-accueil'}>
         {!showimage && isVisible && <h2>Chez vous, partout et ailleurs</h2>}
-        {showimage && isVisible && <img src={require('../../assets/Image source 2 (1).svg').default} alt="Belle vue sur la mer" />}
+        {showimage && isVisible && <img src={bannerImage} alt="Belle vue sur la mer" />}
       </section>
-      </>
-  )
+    </>
+  );
 }
-  
-  export default Banner;
+
+export default Banner;
+
